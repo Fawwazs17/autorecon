a3f953f376b8f2d222cc0af499bcb9655bd2b380
Fawwazs17 <adamfawwazsazalizam@gmail.com>
2025-07-23 23:55:26 +0800

    debug v2

--- scanners/whatweb_scanner.py
+++ scanners/whatweb_scanner.py
@@ -8,9 +8,9 @@ def whatweb_scan(domain):
     command = [
         "whatweb",
         f"https://{domain}",
-        '--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64)"',
-        '--header="Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"',
-        '--header="Accept-Language: en-US,en;q=0.5"',
+        "--user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+        "--header", "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "--header", "Accept-Language: en-US,en;q=0.5",
         f"--log-brief=results/whatweb_{domain}.txt"
     ]
     try:
